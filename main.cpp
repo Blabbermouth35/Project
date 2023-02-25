c7e976650a08d8ab2677f57089ce550773265280
Blabbermouth35 <poyrazmen35@gmail.com>
2023-02-26 02:14:32 +0300

    Learning C++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    int a = 1;
-    int b = 2;
-    std::cout << a;
+    int sales = 95000;
+    double state_tax = sales / 100 * 4;
+    double country_tax = sales / 100 * 2;
+    cout << "sales = " << sales << endl
+         << "State Tax = " << state_tax << endl
+         << "Country Tax = " << country_tax << endl
+         << "Profits = " << sales - state_tax - country_tax;
+    return 0;
 }
